c0804469f638522c9016fb37a4c91699abd3e18b
Max Schnaider <maxschnaider@pm.me>
2022-09-30 21:27:14 +0300

    🔥 Burn entity & logic, search burn by address query

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,18 +1,46 @@
 import { Transfer as TransferEvent } from "../generated/AtlantisWorldFoundingAtlanteanScrolls/AtlantisWorldFoundingAtlanteanScrolls";
-import { Token, User } from "../generated/schema";
+import { Token, User, Burn } from "../generated/schema";
 
 export function handleTransfer(event: TransferEvent): void {
-  let token = Token.load(event.params.tokenId.toString());
+  const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000';
+  /**
+   * params
+   */
+  const tokenIdBI = event.params.tokenId;
+  const tokenId = event.params.tokenId.toString();
+  const from = event.params.from.toHexString();
+  const to = event.params.to.toHexString();
+  const hash = event.transaction.hash.toHexString();
+
+  /**
+   * Token entity
+   */
+  let token = Token.load(tokenId);
   if (!token) {
-    token = new Token(event.params.tokenId.toString());
-    token.tokenId = event.params.tokenId;
+    token = new Token(tokenId);
+    token.tokenId = tokenIdBI;
   }
-  token.owner = event.params.to.toHexString();
-  token.ownerId = event.params.to.toHexString();
+  token.owner = to;
+  token.ownerId = to;
   token.save();
-  let user = User.load(event.params.to.toHexString());
+
+  /**
+   * User entity
+   */
+  let user = User.load(to);
   if (!user) {
-    user = new User(event.params.to.toHexString());
+    user = new User(to);
     user.save();
+  }
+
+  /**
+   * Burn entity
+   */
+  if (to === ADDRESS_ZERO) {
+    const burn = new Burn(tokenId);
+    burn.hash = hash;
+    burn.token = token.id;
+    burn.ownerId = from;
+    burn.save();
   }
 }
